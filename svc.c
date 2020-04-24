4406270d1359217e9c6619a701201d2b28942133
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-04-24 13:07:50 +0800

    push
    push

--- svc.c
+++ svc.c
@@ -78,7 +78,7 @@ int do_count(FILE* f ,int hash){
     char count[255];
     int i;
     while (fscanf(f, "%254s",count) == 1) {
-        count[255] = '\n';
+//        count[255] = '\n';
         for (i = 0; i < strlen(count); i++) {
             hash = (hash + count[i]) % 2000000000;
         }
