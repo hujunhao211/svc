e233ce651fc78e523fb4c94e974456cf52340c00
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-04-25 22:13:08 +0800

    push
    push

--- svc.c
+++ svc.c
@@ -64,10 +64,10 @@ void cleanup(void *helper) {
             free(help->branches[i]->branch_commit[j]->commit_id);
             free(help->branches[i]->branch_commit[j]->branch_p);
             int z;
-            for (z = 0; z < help->branches[i]->branch_commit[j]->file_length;z++){
-                free(help->branches[i]->branch_commit[j]->files_array[z]->file_name);
-                free(help->branches[i]->branch_commit[j]->files_array[z]);
-            }
+//            for (z = 0; z < help->branches[i]->branch_commit[j]->file_length;z++){
+//                free(help->branches[i]->branch_commit[j]->files_array[z]->file_name);
+//                free(help->branches[i]->branch_commit[j]->files_array[z]);
+//            }
             free(help->branches[i]->branch_commit[j]->files_array);
             free(help->branches[i]->branch_commit[j]->message);
             free(help->branches[i]->branch_commit[j]->next);
