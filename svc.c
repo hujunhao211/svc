a9476fd1ad7770217c1ead49ec3a2875da17e237
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-04-26 23:14:08 +0800

    push
    push

--- svc.c
+++ svc.c
@@ -486,13 +486,11 @@ char *svc_commit(void *helper, char *message) {
             for (i = 0; i < help->file_length; i++){
                 FILE* file = fopen(help->file_array[i]->file_name,"r");
                 if (file != NULL){
-                    fclose(file);
                     char* get_name = get_file_name(commit_id);
                     char* free_file = concat("A", get_file_name(hash_file(NULL, help->file_array[i]->file_name)), get_name);
                     copy_file(help->file_array[i]->file_name,free_file);
                     free(free_file);
                 }
-                fclose(file);
             }
             struct commit* pre = help->branch_p->precommit;
             if (pre->next_size == 0){
