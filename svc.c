ba65926fcc414dd300d6ad42214eebd6d38cc7b3
hujunhao211 <61451559+hujunhao211@users.noreply.github.com>
2020-04-30 15:28:29 +0800

    push
    push

--- svc.c
+++ svc.c
@@ -1305,12 +1305,12 @@ int svc_addss(void *helper, char *file_name) {
 //    printf("find_remove %d\n",find_remove);
     if (find_remove){
 //        printf("remove_length %d\n",remove_length);
-        char* filename = array_remove[remove_index];
-        for (i = remove_index; i < remove_length - 1; i++){
-            array_remove[i] = array_remove[i + 1];
-        }
-        free(filename);
-        remove_length--;
+//        char* filename = array_remove[remove_index];
+//        for (i = remove_index; i < remove_length - 1; i++){
+//            array_remove[i] = array_remove[i + 1];
+//        }
+//        free(filename);
+//        remove_length--;
     } else {
 //        printf("add2 %s\n",file_name);
         if (help->file_length == help->capacity){
